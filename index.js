f247b7e722c8cf51b806802a658c372bc32dd38c
abdulh70 <abdulazizh70@gmail.com>
2024-10-01 11:21:09 +0300

    submitted string lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Abdulaziz';
+let welcomeMessage='Welcome to Flatbook, '+currentUser+"!";
+let excitedWelcomeMessage=`WELCOME TO FLATBOOK, `+ currentUser.toUpperCase()+"!";
+const shortGreeting = ' "Welcome, "'+currentUser.charAt(0)+"!";
+
